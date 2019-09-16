65e2909f9be15782efce617330aef5454f888600
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2019-09-16 14:48:51 +0300

    First commit

---
+++ setlist/src/main/java/palvelinohjelmointi/setlist/SetlistApplication.java
@@ -0,0 +1,13 @@
+package palvelinohjelmointi.setlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetlistApplication.class, args);
+	}
+
+}
